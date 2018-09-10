90aa161737f38afaced74e14de0d2b78802084b4
xiaoyuancai <18101372070@163.com>
2018-09-10 16:50:39 +0800

    mac 下添加 访问图像像素功能的三种示例

--- README.md
+++ README.md
@@ -3,3 +3,68 @@
 #####iOS项目中：
 注意添加opencv2.framework，添加方式已经在[博客](https://www.jianshu.com/p/c54c887dc2f7)中说明，由于文件太大，我这里没有上传。
 
+#####学习OpenCV基本结构->Mat结构
+1. 我们可以简单认为Mat是OpenCV中的图片
+2. 矩阵数据结构
+3. Mat同时是一个C++类
+
+#####总结一下->Mat结构创建方式有哪些
+######方式一：创建Mat->构造方法创建
+1. Mat mat_image_dst(mat_image_src.rows, mat_image_src.cols, CV_8UC3);
+2. Mat mat_image_dst(Size(mat_image_src.rows, mat_image_src.cols), CV_8UC3);
+3. Mat mat_image_dst(200, 200, CV_8UC3, Scalar(255, 0, 0));
+4. Mat mat_image_dst(Size(300, 300), CV_8UC3, Scalar(255, 0, 0));
+5. Mat mat_image_dst(mat_image_src);
+6. Mat mat_image_dst(mat_image_src, Rect(0, 0, 100, 100));
+######方式二：创建Mat->create成员函数（方法）代码如下
+Mat mat(100, 100, CV_8UC3, Scalar(0, 0, 255));
+mat.create(50, 50, CV_8UC(3));
+```
+运行结果：发现一片漆黑，没有任何颜色（存在问题）注意事项：这个create函数起始不能够为Mat矩阵设置初始值，但是可以在尺寸发生改变的时候重新为矩形数据开辟内存空间
+```
+
+######方式三：创建Mat->Matlab初始化（库）代码如下
+Mat mat = Mat::eye(100, 100, CV_64F);
+Mat mat = Mat::ones(100, 100, CV_64F);
+Mat mat = Mat::zeros(100, 100, CV_64F);
+eye：表示缩放操作
+ones：表示任何一个数组然后初始化Mat
+zeros：表示用这个函数可以创建一个作为参数的一个矩阵初始化器->初始化器（默认值：0）
+######方式四：对小Mat矩阵通过逗号分隔符方式初始化
+Mat mat = (Mat_<int>(2, 2) << 0, 3, -1, 0);
+######方式五：通过clone函数和copyTo函数作为一个已经存在Mat对象，创建一个新的Mat对象
+Mat mat_image_clone = mat_image_src.clone();
+Mat mat_image_clone;
+mat_image_src.copyTo(mat_image_clone);
+
+#####3. OpenCV开发中->常用数据结构
+1. 第一个->Point类->表示点
+	第一种：二维点
+	Point2f p2f(100, 100);
+	
+	第二种：三维点
+	Point3f p3f(100, 100, 100);
+2. 第二个->Scalar类->常用于表示颜色
+	RGB值，就是三个参数代码如下
+	Scalar(0, 0, 255);
+	参数一：蓝色->B
+	参数二：绿色->G
+	参数三：红色->R			
+```
+Scalar基是Matx: Matx设计目的：用于存储小图片（像素点->RGB、BGR）->小矩阵（轻量级Mat）
+做OpenCV开发将Mat->Matx，Matx->Mat
+```
+3. 第三个->Size类->表示尺寸大小（说白了：表示图片大小）Size(100, 100); 
+4. 第四个->Rect类->矩形类型（长方形、正方形等等…）
+	Rect rect(0, 0, 100, 100);
+	常用函数
+rect.size()->返回矩形宽高（区域）
+rect.area()->返回矩形面积->width * height
+rect.contains()->判断某个点是否在矩形范围之内
+rect.tl()->返回左上角点的坐标
+rect.br()->返回右下角点的坐标
+rect1 & rect2 ->表示两个矩形交集
+ect1 | rect2 ->表示两个矩形并集
+ect + Point(50, 50)->表示矩形平移操作
+rect + Size(50, 50)->表示矩形缩放操作
+
--- openCV_demo_mac/openCV_demo_mac/main.cpp
+++ openCV_demo_mac/openCV_demo_mac/main.cpp
@@ -138,26 +138,147 @@ using namespace std;
 //}
 
 //案例五：图片模糊
-int main( int argc, char** argv ){
-    
-    //第一步：加载图片
-    //imread函数：读取文件->返回一个Mat矩阵(是OpenCV框架最基本数据类型)
+//int main( int argc, char** argv ){
+//
+//    //第一步：加载图片
+//    //imread函数：读取文件->返回一个Mat矩阵(是OpenCV框架最基本数据类型)
+//    Mat mat_image_src = imread("/Users/yuanle/Desktop/image1.jpg");
+//
+//    //第二步：将图片进行灰度处理
+//    Mat mat_gary_img;
+//    cvtColor(mat_image_src,mat_gary_img, COLOR_BGR2GRAY);
+//
+//    //第三步：使用3*3降噪处理
+//    Mat mat_edge_img;
+//    blur(mat_gary_img, mat_edge_img, Size(3,3));
+//    //第四步：算子（一种算法）处理
+//    Canny(mat_edge_img, mat_edge_img, 3, 9,3);
+//
+//    //第五步：显示图片
+//    imshow("result", mat_edge_img);
+//    waitKey();
+//
+//    return 0;
+//
+//}
+
+
+//案例六：访问图像像素
+void colorSpaceReduce(Mat &mat_image_in, Mat &mat_image_out, int div);
+int main(int argc, const char * argv[]) {
+
+    //第一步：创建一张图片
     Mat mat_image_src = imread("/Users/yuanle/Desktop/image1.jpg");
-    
-    //第二步：将图片进行灰度处理
-    Mat mat_gary_img;
-    cvtColor(mat_image_src,mat_gary_img, COLOR_BGR2GRAY);
-    
-    //第三步：使用3*3降噪处理
-    Mat mat_edge_img;
-    blur(mat_gary_img, mat_edge_img, Size(3,3));
-    //第四步：算子（一种算法）处理
-    Canny(mat_edge_img, mat_edge_img, 3, 9,3);
-    
-    //第五步：显示图片
-    imshow("result", mat_edge_img);
+
+    //第二步：按照原始图片创建一张新的图片
+    Mat mat_image_dst;
+    mat_image_dst.create(mat_image_src.rows,
+                         mat_image_src.cols,
+                         mat_image_src.type());
+
+    //记录开始时间
+    //getTickCount()函数表示：返回CPU自某个事件以来走过的时间周期数(纳秒，非常小的时间单位)
+    //例如：电脑启动、电脑休眠等等...
+    double time_start = static_cast<double>(getTickCount());
+
+    //第三步：颜色空间缩减
+    colorSpaceReduce(mat_image_src, mat_image_dst, 100);
+
+    //记录结束时间
+    double time_end = static_cast<double>(getTickCount());
+    //getTickFrequency()函数表示：返回CPU一秒钟所有的时间周期数，这样的话是不是我就能够按照秒单位计算出耗时
+    double time = (time_end - time_start) / getTickFrequency();
+    printf("耗时%f秒", time);
+
+
+    //第三步：显示图片
+    namedWindow("图片");
+    imshow("pic", mat_image_dst);
     waitKey();
-        
     return 0;
-    
 }
+
+
+//第一类：指针访问
+void colorSpaceReduce(Mat &mat_image_in, Mat &mat_image_out, int div){
+    mat_image_out = mat_image_in.clone();
+    int rows = mat_image_out.rows;
+    int cols = mat_image_out.cols;
+    //通道数量->2种颜色，3种颜色（每一个像素点，有多少个颜色）
+    int channels = mat_image_out.channels();
+    //获取每一行元素个数->RGB个数
+    int colsSize = cols * channels;
+
+    //循环缩减颜色通道
+    for (int i = 0; i < rows; i++) {
+        //获取第i行->指针访问像素点
+        uchar* data = mat_image_out.ptr<uchar>(i);
+        for (int j = 0; j < colsSize; j++) {
+            //指针位移
+            //data[j]->表示一个颜色值
+            //data[j] = 100
+            //div = 24
+            //data[j] = 100 / 24 * 24 = 96
+            //data[j] = 96 + 24 / 2 = 108
+            //这只是一个公式而已(随便你改)->想怎么玩就怎么玩->随心所欲
+            data[j] = data[j] / div * div;
+        }
+    }
+}
+
+//第二类：迭代器访问->封装东西比较多(根据场景)
+//void colorSpaceReduce(Mat &mat_image_in, Mat &mat_image_out, int div){
+//    mat_image_out = mat_image_in.clone();
+//    Mat_<Vec3b>::iterator it_start = mat_image_out.begin<Vec3b>();
+//    Mat_<Vec3b>::iterator it_end = mat_image_out.end<Vec3b>();
+//
+//    //循环缩减颜色通道
+//    for (; it_start != it_end; it_start++) {
+//        //(*it_start)像素点
+//        //(*it_start)[0]：表示获取像素点值->蓝色->B值
+//        (*it_start)[0] = (*it_start)[0] / div * div;
+//        (*it_start)[1] = (*it_start)[1] / div * div;
+//        (*it_start)[2] = (*it_start)[2] / div * div;
+//    }
+//}
+
+//第三类：动态地址计算
+//void colorSpaceReduce(Mat &mat_image_in, Mat &mat_image_out, int div){
+//    mat_image_out = mat_image_in.clone();
+//    int rows = mat_image_out.rows;
+//    int cols = mat_image_out.cols;
+//
+//    for (int i = 0; i < rows; i ++) {
+//        for (int j = 0; j < cols; j++) {
+//            mat_image_out.at<Vec3b>(i, j)[0] = mat_image_out.at<Vec3b>(i, j)[0] / div * div;
+//            mat_image_out.at<Vec3b>(i, j)[1] = mat_image_out.at<Vec3b>(i, j)[1] / div * div;
+//            mat_image_out.at<Vec3b>(i, j)[2] = mat_image_out.at<Vec3b>(i, j)[2] / div * div;
+//        }
+//    }
+//}
+
+//案例七：ROI感兴趣区域
+//int main(int argc, const char * argv[]) {
+//
+//    //第一步：创建一张图片
+//    Mat mat_image_src = imread("/Users/yuanle/Desktop/image2.jpg");
+//    Mat mat_image_logo = imread("/Users/yuanle/Desktop/image3.jpg");
+//
+//    //第二步：灰度处理->加载一张灰度图片
+//    Mat mat_image_mask = imread("/Users/yuanle/Desktop/image3.jpg");
+//
+//    //第三步：定义ROI区域
+//    //根据原始图片->得到src图片一块内存地址(指针)
+//    Mat mat_image_roi = mat_image_src(Rect2i(0, 0, mat_image_logo.cols, mat_image_logo.rows));
+//
+//    //第三步：拷贝到ROI区域
+//    //参数一：ROI区域
+//    //参数二：需要填充图片(水印)
+//    mat_image_logo.copyTo(mat_image_roi, mat_image_mask);
+//
+//    //第三步：显示图片
+//    namedWindow("图片");
+//    imshow("图片", mat_image_src);
+//    waitKey();
+//    return 0;
+//}
